f2b886c533c1e79e27c16fac18547d1cdf70354a
VictorYXL <hllyq10@163.com>
2018-01-23 23:26:28 +0800

    Try

--- AdaBoost/HorseColic.py
+++ AdaBoost/HorseColic.py
@@ -12,9 +12,9 @@ def LoadDataset(fileName):
 
 if __name__ == '__main__':
     trainingDataArray, trainingLabelList = LoadDataset("HorseColicTraining.txt")
-    classifierList, totalPredictValue = AdaBoost.AdaboostTrain(trainingDataArray, trainingLabelList, 10)
+    classifierList, totalPredictValue = Ada    Boost.AdaboostTrain(trainingDataArray, trainingLabelList, 10)
     testDataArray, testLabelList = LoadDataset("HorseColicTest.txt")
     result = AdaBoost.AdaClassify(testDataArray, classifierList)
     errorList = [i for i in range(len(testLabelList)) if testLabelList[i] != result[i]]
     AdaBoost.PlotROC(trainingLabelList, totalPredictValue)
-    #print (errorList)
+    #print (errorList) 
