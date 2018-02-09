45e235123f33b613a5599068f259f7f9b25b32db
VictorYXL <hllyq10@163.com>
2018-02-09 20:41:49 +0800

    Plot Regression

--- Regression/StandRegres.py
+++ Regression/StandRegres.py
@@ -1,4 +1,5 @@
-from numpy import *
+import numpy 
+import matplotlib.pyplot
 
 def LoadDataset(fileName):
     file = open(fileName)
@@ -12,14 +13,31 @@ def LoadDataset(fileName):
 
 # w = (X.T * X)^(-1) * (X.T * Y)
 def StandRegres(dataArray, labelList):
-    dataMat = mat(dataArray)
-    labelMat = mat(labelList)
+    dataMat = numpy.mat(dataArray)
+    labelMat = numpy.mat(labelList)
 
-    if linalg.det(dataMat.T * dataMat) == 0:
+    if numpy.linalg.det(dataMat.T * dataMat) == 0:
         print("No x^(-1)")
         return
-
     return (dataMat.T * dataMat).I * (dataMat.T * labelMat.T)
 
+#scatter: point, scatter(x array, y array)
+#plot: line, plot([x0, x1, x2], [y0, y1, y2])
+
+def Plot(x, y, w):
+    fig = matplotlib.pyplot.figure()
+    ax = matplotlib.pyplot.subplot(111)
+    ax.scatter(x, y, c = 'blue')
+    
+    x1 = min(x)
+    y1 = w[0] + x1 * w[1]
+    x2 = max(x)
+    y2 = w[0] + x2 * w[1]
+    ax.plot([x1, x2], [y1, y2], 'r-')
+
+    matplotlib.pyplot.show()
+
 dataArray, labelList = LoadDataset('DataSet1.txt')
-print(StandRegres(dataArray, labelList))
+w = StandRegres(dataArray, labelList)
+x = [i[1] for i in dataArray]
+Plot(x, labelList, w.flatten().A[0])
