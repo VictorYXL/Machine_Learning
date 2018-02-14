3a41955c0cf610892045bc6b3d1a6b75d273dab7
VictorYXL <hllyq10@163.com>
2018-02-14 16:29:41 +0800

    Ridge and Stage Regression

--- Regression/Regression.py
+++ Regression/Regression.py
@@ -15,6 +15,8 @@ def LoadDataset(fileName):
 def StandRegres(dataArray, labelList):
     dataMat = numpy.mat(dataArray)
     labelMat = numpy.mat(labelList)
+    dataMat = (dataMat - numpy.mean(dataMat, 0)) / numpy.var(dataMat, 0)
+    labelMat = labelMat - numpy.mean(labelMat)
 
     if numpy.linalg.det(dataMat.T * dataMat) == 0:
         print("No x^(-1)")
@@ -25,6 +27,8 @@ def StandRegres(dataArray, labelList):
 def LocalWeightLinearRegress(testX, dataArray, labelList, k = 1.0):
     dataMat = numpy.mat(dataArray)
     labelMat = numpy.mat(labelList)
+    dataMat = (dataMat - numpy.mean(dataMat, 0)) / numpy.var(dataMat, 0)
+    labelMat = labelMat - numpy.mean(labelMat)
     weight = numpy.zeros((len(dataArray), len(dataArray)))
 
     for i in range(len(dataArray)):
@@ -34,25 +38,84 @@ def LocalWeightLinearRegress(testX, dataArray, labelList, k = 1.0):
         print("No x^(-1)")
         return
     return numpy.mat(testX) * (dataMat.T * (weight * dataMat)).I * (dataMat.T * (weight * labelMat.T)) 
+
+
+# w = (X.T * X + lam * I)^(-1) * (X.T * Y)
+def RidgeRegres(dataArray, labelList, lam = 0.2):
+    dataMat = numpy.mat(dataArray)
+    labelMat = numpy.mat(labelList)
+    dataMat = (dataMat - numpy.mean(dataMat, 0)) / numpy.var(dataMat, 0)
+    labelMat = labelMat - numpy.mean(labelMat)
+    I = numpy.eye(numpy.shape(dataMat)[1])
+
+    if numpy.linalg.det(dataMat.T * dataMat + lam * I) == 0:
+        print("No x^(-1)")
+        return
+    return (dataMat.T * dataMat + lam * I).I * (dataMat.T * labelMat.T)
+
+def StageWise(dataArray, labelList, esp = 0.01, numIt = 1000):
+    dataMat = numpy.mat(dataArray)
+    labelMat = numpy.mat(labelList)
+    dataMat = (dataMat - numpy.mean(dataMat, 0)) / numpy.var(dataMat, 0)
+    labelMat = labelMat - numpy.mean(labelMat)
+
+    w = numpy.zeros((1, len(dataArray[0])))
+    minError = numpy.inf
+
+    #Iteration times
+    for i in range(numIt):
+        #Modify weight
+        for j in range(len(dataArray[0])):
+            #Add or substract
+            for k in [-1, 1]:
+                newW = w.copy()
+                newW[0, j] += k * esp
+                predLabel = dataMat * newW.T
+                predError = RegError(labelList, predLabel.flatten().tolist()[0])
+                #New best weight
+                if (predError < minError):
+                    minError = predError
+                    w = newW.copy()
+                    print(w, minError)
+    return w
+
+def RegError(labelList, predLabelList):
+    diffList = [(labelList[i] - predLabelList[i]) ** 2 for i in range(len(labelList))]
+    return sum(diffList)
+
 #scatter: point, scatter(x array, y array)
 #plot: line, plot([x0, x1, x2], [y0, y1, y2])
-def Plot(x, y, w):
+def Plot(X, Y, w, LWLRPredY):
     fig = matplotlib.pyplot.figure()
     ax = matplotlib.pyplot.subplot(111)
-    ax.scatter(x, y, c = 'blue')
+    ax.scatter(X, Y, c = 'blue')
+    #ax.scatter(X, LWLRPredY, c = 'green')
     
-    x1 = min(x)
+    x1 = min(X)
     y1 = w[0] + x1 * w[1]
-    x2 = max(x)
+    x2 = max(X)
     y2 = w[0] + x2 * w[1]
     ax.plot([x1, x2], [y1, y2], 'r-')
+
+    sortedXIndex = numpy.argsort(X)
+    for index in range(len(X) - 1):
+        ax.plot([X[sortedXIndex[index]], X[sortedXIndex[index + 1]]], [LWLRPredY[sortedXIndex[index]], LWLRPredY[sortedXIndex[index + 1]]], 'g--')
     matplotlib.pyplot.show()
 
-dataArray, labelList = LoadDataset('DataSet1.txt')
-#w = StandRegres(dataArray, labelList)
-print(LocalWeightLinearRegress([1.000000, 0.067732], dataArray, labelList, 1.0))
-print(LocalWeightLinearRegress([1.000000, 0.067732], dataArray, labelList, 0.1))
-print(LocalWeightLinearRegress([1.000000, 0.067732], dataArray, labelList, 0.01))
-print(LocalWeightLinearRegress([1.000000, 0.067732], dataArray, labelList, 0.001))
-#x = [i[1] for i in dataArray]
-#Plot(x, labelList, w.flatten().A[0])
+
+#dataArray, labelList = LoadDataset('Dataset1.txt')
+dataArray, labelList = LoadDataset('Dataset2.txt')
+
+print(StageWise(dataArray, labelList, esp = 0.001, numIt = 10000))
+print(StandRegres(dataArray, labelList))
+
+# w = StandRegres(dataArray, labelList)
+# StandPredY = [(w[0] + dataArray[i][1] * w[1]).tolist()[0][0] for i in range(len(dataArray))]
+# LWLRPredY = [LocalWeightLinearRegress(data, dataArray, labelList, 0.01).tolist()[0][0] for data in dataArray]
+# w1 = RidgeRegres(dataArray, labelList)
+# RidgePredY = [(w1[0] + dataArray[i][1] * w1[1]).tolist()[0][0] for i in range(len(dataArray))]
+# x = [i[1] for i in dataArray]
+# Plot(x, labelList, w.flatten().A[0], LWLRPredY)
+# print(RegError(labelList, StandPredY))
+# print(RegError(labelList, RidgePredY))
+# print(RegError(labelList, LWLRPredY))
