14106fab5fc27407624e45f7ebb3a83dee68af9a
VictorYXL <hllyq10@163.com>
2018-02-24 00:08:12 +0800

    K Means First

---
+++ K Means/KMeans.py
@@ -0,0 +1,70 @@
+import sys
+sys.path.append("../Basic Functions")
+from LoadData import LoadData
+import numpy
+import matplotlib.pyplot
+import random
+
+#Calculate the distance
+def Distance(vec1 ,vec2):
+    return ((vec1 - vec2) * (vec1 - vec2).T)[0,0]
+
+#Create k center points
+def CreateCenter(dataMat, k):
+    featureCount = len(dataArray[0])
+    centerPointMat = numpy.zeros((k, featureCount))
+
+    for i in range(featureCount):
+        minI = min(dataMat[:, i])
+        maxI = max(dataMat[:, i])
+        for j in range(k):
+            centerPointMat[j, i] = random.uniform(minI, maxI)
+    return centerPointMat
+
+#K-Means cluster
+def KMeans(dataArray, k):
+    dataCount = len(dataArray)
+    featureCount = len(dataArray[0])
+    dataMat = numpy.mat(dataArray)
+    centerPointMat = CreateCenter(dataMat, k)
+    #Record the Cluster Assment and each distance as: clusterAssment[data_index] = [belonged_cluster, distance]
+    clusterAssment = [[0, 0] for i in range(dataCount)]
+    whetherChanged = True
+
+    while whetherChanged:
+        whetherChanged = False
+        for (index, data) in enumerate(dataMat):
+            DistanceList = [Distance(data, centerPoint) for centerPoint in centerPointMat]
+            #Find min distance and use its index as new cluster
+            newDis = min(DistanceList)
+            newCluster = DistanceList.index(newDis)
+            if (clusterAssment[index][0] != newCluster):
+                whetherChanged = True
+                #Update the assment
+                clusterAssment[index][0] = newCluster
+                clusterAssment[index][1] = newDis
+                #Update the cluster
+                dataInCluster = [dataMat[index] for index in range(dataCount) if clusterAssment[index][0] == newCluster]
+                centerPointMat[newCluster] = numpy.mean(dataInCluster, 0)
+    return centerPointMat, clusterAssment
+
+def Plot(dataArray, centerPointMat, clusterAssment):
+    fig = matplotlib.pyplot.figure()
+    ax = matplotlib.pyplot.subplot(111)
+    color = ['blue', 'red', 'yellow', 'black', 'gray', 'green', 'gray']
+
+    for i in range(len(centerPointMat)):
+        X = [data[0] for (index, data) in enumerate(dataArray) if clusterAssment[index][0] == i]
+        Y = [data[1] for (index, data) in enumerate(dataArray) if clusterAssment[index][0] == i]
+        ax.scatter(X, Y, c = color[i])
+        ax.scatter(centerPointMat[i, 0], centerPointMat[i, 1], c = color[i], marker = 'x')
+    matplotlib.pyplot.show()
+
+
+
+
+if __name__ == '__main__':
+    dataArray = LoadData("Dataset.txt")
+    centerPointMat, clusterAssment = KMeans(dataArray, 4)
+    print(centerPointMat)
+    Plot(dataArray, centerPointMat, clusterAssment)
