0a711a3dd1253cb8e4b3c034aaafe8165f1f442f
VictorYXL <hllyq10@163.com>
2018-03-05 23:34:35 +0800

    Find Apriori

--- Apriori/Apriori.py
+++ Apriori/Apriori.py
@@ -7,26 +7,54 @@ def CreateCanSet(dataArray):
                 canSet.append([item])
     canSet.sort()
     return canSet
+
 #Return support item that frequency is higher that min support
 def SuportItemList(dataArray, canSet, minSupport):
-    frequencyList = [0 for i in  range(len(canSet))]
+    tmpFreList = [0 for i in  range(len(canSet))]
     for data in dataArray:
         for (index, item) in enumerate(canSet):
-            #set(a) > set(b): a includes b
-            if set(data) > set(item):
-                frequencyList[index] = frequencyList[index] + 1 / len(dataArray)
+            #set(a) >= set(b): a includes b
+            if set(data) >= set(item):
+                tmpFreList[index] = tmpFreList[index] + 1 / len(dataArray)
     
     supportList = []
-    for (index, freq) in enumerate(frequencyList):
+    frequencyList = []
+    for (index, freq) in enumerate(tmpFreList):  
         if freq >= minSupport:
-            supportList.append(canSet[index])        
+            supportList.append(canSet[index])
+            frequencyList.append(tmpFreList[index])
+            
     return supportList, frequencyList
+
+#From list get cartesian product
+def carProduct(basicList):
+    outputlist = []
+    for i in range(len(basicList)):
+        for j in range(i + 1, len(basicList)):
+            if list(set(basicList[i] + basicList[j])) not in outputlist:
+                outputlist.append(list(set(basicList[i] + basicList[j])))
+    return outputlist
+
+def Apriori(dataArray, minSupport):
+    canSet = CreateCanSet(dataArray)
+    currentSupportList, currentFrequencyList = SuportItemList(dataArray, canSet, minSupport)
+    supportList = [currentSupportList]
+    frequencyList = [currentFrequencyList]
+    canListLen = 0
+    while len(supportList[canListLen]) > 0:
+        canSet = carProduct(currentSupportList)
+        currentSupportList, currentFrequencyList = SuportItemList(dataArray, canSet, minSupport)
+        if len(currentFrequencyList) <= 0:
+            break
+        supportList.append(currentSupportList)
+        frequencyList.append(currentFrequencyList)
+        canListLen = canListLen + 1
+    return supportList, frequencyList
+
 
 
 if __name__ == '__main__':
     dataArray = [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
-    canSet = CreateCanSet(dataArray)
-    supportList, frequencyList = SuportItemList(dataArray, canSet, 0.5)
+    supportList, frequencyList = Apriori(dataArray, 0.5)
     print(supportList)
     print(frequencyList)
-
