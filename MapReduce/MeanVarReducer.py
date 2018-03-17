82e164bbae3e7e0584bf502c0610dc04d9845f66
VictorYXL <hllyq10@163.com>
2018-03-17 13:45:21 +0800

    Debug in var

--- MapReduce/MeanVarReducer.py
+++ MapReduce/MeanVarReducer.py
@@ -7,11 +7,10 @@ def MeanAndVarReducer(lines):
     count = 2
     for line in lines:
         curMean, curVar, curCount = map(float, line.split(" "))
-        方差公式写的不对。。明天重算
         mean = (mean * count + curMean * curCount) / (count + curCount)
-        var = var + curVar * curCount + 2 * curMean * curMean * curCount - curMean * curMean
+        var = var + curVar * curCount + 2 * curMean * curMean * curCount - curMean * curMean * curCount
         count = count + curCount
-    var = (var - 2 * mean * mean * curCount + curMean * curMean) / (count + curCount)
+    var = (var - 2 * mean * mean * count + mean * mean * count) / count
     return mean, var, count
 if __name__ == '__main__':
     lines = sys.stdin.readlines()
